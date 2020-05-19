8a5c8d84a6962aa176c7865598ebb818e968df73
drum-computer <densi.rhythm@gmail.com>
2020-05-19 16:17:21 +0300

    PITCHKNOBS: creagted container to hold all pitchknobs

---
+++ td project/scripts/PitchKnobsClass.py
@@ -0,0 +1,24 @@
+"""
+Extension classes enhance TouchDesigner components with python. An
+extension is accessed via ext.ExtensionClassName from any operator
+within the extended component. If the extension is promoted via its
+Promote Extension parameter, all its attributes with capitalized names
+can be accessed externally, e.g. op('yourComp').PromotedFunction().
+
+Help: search "Extensions" in wiki
+"""
+
+from TDStoreTools import StorageManager
+TDF = op.TDModules.mod.TDFunctions
+
+class PitchKnobs:
+	"""
+	PitchKnobs description
+	"""
+	def __init__(self, ownerComp):
+		# The component to which this extension is attached
+		self.ownerComp = ownerComp
+		self.pitchValues = [knob.Value for knob in ops('knob*')]
+		# properties
+		TDF.createProperty(self, 'Pitch', value=self.pitchValues, dependable=True,
+						   readOnly=False)
